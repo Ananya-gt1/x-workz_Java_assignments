9d847015a54fe7be6a338845a915e8f4975fbfe2
Ananya G T <ananyathippeswamy7353@gmail.com>
2025-03-24 10:52:52 -0700

    updating assignment

--- Instance variables and instance methods/src/com/xworkz/assignment3/Minister.java
+++ Instance variables and instance methods/src/com/xworkz/assignment3/Minister.java
@@ -1,22 +1,32 @@
 package com.xworkz.assignment3;
 
 public class Minister {
-    public String name;
-    int age;
-    private String state;
+    public String ministernName;
+    int ministerAge;
+    private String ministerState;
+    public String primeministerName;
+    int primeministerAge;
+    private String primeministerState;
+    public String mlaName;
+    int mlaAge;
+    private String mlaState;
+
 
   public  Minister(String name, int age,String state){
         this.name=name;
         this.age=age;
         this.state=state;
+        this.primeministerName=primeministerName;
     }
     public void minister(){
-        System.out.println("Name:"+this.name+", Age:"+this.age+", State:"+this.state);
+        System.out.println("Public method \nName:"+this.name+", Age:"+this.age+", State:"+this.state);
+        mla();
+        primeMinister();
     }
     void primeMinister(){
-        System.out.println("Name:"+this.name+", Age:"+this.age+", State:"+this.state);
+        System.out.println("Default method \nName:"+this.name+", Age:"+this.age+", State:"+this.state);
     }
     private void mla(){
-        System.out.println("Name:"+this.name+", Age:"+this.age+", State:"+this.state);
+        System.out.println("Private method \nName:"+this.name+", Age:"+this.age+", State:"+this.state);
     }
 }
